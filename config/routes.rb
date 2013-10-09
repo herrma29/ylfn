0cbed9b4091116183b29ac56b8ffc4b29ebfa492
Chris <chris@ubuntu.(none)>
2013-10-09 02:02:59 -0400

    added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Ylfn::Application.routes.draw do
-  root to: "weclome#index"
+  match '/help', to: "static_pages#help", via: 'get'
+  match '/about', to: "static_pages#about", via: 'get'
+  match '/contact', to: "static_pages#contact", via: 'get'
+  root to: "static_pages#index"
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
